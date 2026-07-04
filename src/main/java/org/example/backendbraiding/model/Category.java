fd28f760b5f3251b150bc64a5d4dc62fba91c5f0
gdjonret <djonretglo@gmail.com>
2026-07-03 19:32:37 -0700

    Add missing JsonIgnore import to fix compilation error

--- src/main/java/org/example/backendbraiding/model/Category.java
+++ src/main/java/org/example/backendbraiding/model/Category.java
@@ -1,5 +1,6 @@
 package org.example.backendbraiding.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
