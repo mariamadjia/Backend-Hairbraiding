1edda56dfb3980e8e0bde2871b39426ad8ceca54
gdjonret <djonretglo@gmail.com>
2026-07-03 20:23:13 -0700

    Add missing ArrayList import to fix compilation error

--- src/main/java/org/example/backendbraiding/service/CategoryService.java
+++ src/main/java/org/example/backendbraiding/service/CategoryService.java
@@ -14,6 +14,7 @@ import org.example.backendbraiding.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
