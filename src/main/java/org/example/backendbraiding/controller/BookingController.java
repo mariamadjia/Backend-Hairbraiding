93c7b2fe47e4de4e16573aab35d48c637dbd138d
gdjonret <djonretglo@gmail.com>
2026-07-16 08:40:00 -0700

    Update BookingController.java

--- src/main/java/org/example/backendbraiding/controller/BookingController.java
+++ src/main/java/org/example/backendbraiding/controller/BookingController.java
@@ -99,6 +99,7 @@ public class BookingController {
                     bookingItem.put("notes", item.getNotes());
                     bookingItem.put("image", item.getImage());
                     bookingItem.put("images", item.getImages());
+                    bookingItem.put("sizePhotos", item.getSizePhotos());
                     bookingItem.put("link", item.getLink());
                     bookingItem.put("objectPosition", item.getObjectPosition());
                     bookingItem.put("availableSizes", item.getAvailableSizes());
@@ -206,6 +207,7 @@ public class BookingController {
                 bookingItem.put("notes", item.getNotes());
                 bookingItem.put("image", item.getImage());
                 bookingItem.put("images", item.getImages());
+                bookingItem.put("sizePhotos", item.getSizePhotos());
                 bookingItem.put("link", item.getLink());
                 bookingItem.put("objectPosition", item.getObjectPosition());
                 bookingItem.put("availableSizes", item.getAvailableSizes());
